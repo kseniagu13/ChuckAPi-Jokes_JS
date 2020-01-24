616f2c4b8fa94934e86a40ffc27e21ba61de72eb
Ksenia Gustsenko <kseniagustsenko@Ksenias-MacBook-Air.local>
2020-01-24 10:39:49 +0200

    proxy

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ function getJokes(event){ //eventi asemel tavaliselt kirjutatakse e, see tähend
     //console.log("button clicked");
     const userNumber = document.querySelector('input[type="number"]').value;
     //console.log(userNumber); kui lisame console.log(typeof(userNumber)); siis näeme, et väljastab tegelt string tüüpi andmed mida oelks vaja konverteerida
-
+    //add proxy enne url et läbi proxy kutsume
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
     //GET request
-    fetch(`http://api.icndb.com/jokes/random/${userNumber}`)
+    fetch(`${proxyUrl}http://api.icndb.com/jokes/random/${userNumber}`)
     .then(function(response){ //paneme selle koodi käima kui response saame kätte
         console.log(response);
         return response.json();
